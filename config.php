2de118fba24e33eb39f8b155b3554c4b751f1332
royal-media-development <info@royal-media-development.de>
2016-11-30 11:01:59 +0100

    PHP STRUKTUR AUFBAUEN

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$PAGE_TITLE = "6PUN";
+$HOST = "localhost";
+$DBNAME = "6pun";
+$DBUSER = "root";
+$DBPASSWORD = "123456";
---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+/**
+ * Diese Datei in jedes Php Dokument einbinden
+ */
+/**
+ * GLOBALE VARS
+ */
+include "config.php";
+
+/**
+ * HEADER
+ */
+include "header.php";
+
+/**
+ * FOOTER
+ */
+include "footer.php";
+
+/**
+ *  FUNKTIONEN
+ */
