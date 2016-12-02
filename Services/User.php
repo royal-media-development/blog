10ecfbfec7b5b5e3de1a61b300209e098f77d27c
royal-media-development <info@royal-media-development.de>
2016-12-02 22:43:59 +0100

    User Klasse hinzugefügt

---
+++ Services/User.php
@@ -0,0 +1,63 @@
+<?php
+namespace Services\User;
+
+
+class User
+{
+    private $id;
+    private $username;
+    private $password;
+    private $img;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImg()
+    {
+        return $this->img;
+    }
+
+    /**
+     * @param mixed $img
+     */
+    public function setImg($img)
+    {
+        $this->img = $img;
+    }
+
+
+}
