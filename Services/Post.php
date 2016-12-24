54b8d1278890fad6f70f9fe65530e44a1cff05aa
Noah Pfeffel <noah.pfeffel@web.de>
2016-12-24 13:02:22 +0100

    vergessen zu speichern

--- Services/Post.php
+++ Services/Post.php
@@ -6,8 +6,14 @@ require_once 'Services/User.php';
 		private $postdate;
 		private $img;
 		
-		public function __construct(User $user){
-			$result = $this->getPostInDB($iser)
+		public function __construct($postID){
+			$result = $this->getPostInDB();
+			if($result) {
+				$this->setPostID($result["id"]);
+				$this->setUserID($result["post"]);
+				$this->setPostDate($result["user"]);
+				$this->setImg($result["value"]);
+			}
 		}
 			
 		public function getPostID(){
@@ -41,4 +47,21 @@ require_once 'Services/User.php';
 		public function setImg($img){
 			$this->img=$img;
 		}
+		
+		private function getPostInDB(){
+			try{
+				include '/../config.php';
+				$conn = new PDO("mysql:host=$HOST;dbname=$DBNAME , $DBCONTENT , $DBCONTENTTIME , $DBUSERID , $DBPOSTID");
+				$conn ->setAttribute(PDO :: ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
+				$stmt = $conn ->prepare("SELECT * FROM POST WHERE postID = $postID");
+				$stmt ->execute();
+				$result = $stmt->fetch();
+			}catch(PDOException $e){
+				$result =$e;
+			}
+			$conn = null;
+		
+			return $result;
+		}
+		
 	}
