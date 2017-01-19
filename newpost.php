602eb126954cf2d706f1962b5b7413cbe4f4e6d4
Noah Pfeffel <noah.pfeffel@web.de>
2017-01-19 10:42:27 +0100

    follow up commit

--- newpost.php
+++ newpost.php
@@ -11,18 +11,13 @@
 	if($session->isUserLoggedIn()){
 		if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
 			$user = $session->getUserSession();
-			$post = new Post();
-			$post->setUserId($user->getId());
-			$post->setImg($filename);
-			$postdate = getCurrentDateTime();
-			$post->setPostDate($postdate);
+			$userid = $user->getId();
 			
 			$connection = new Connection();
-			$userid = $post->getUserId();
-			$connection->setInsert("INSERT INTO POST (user_id, postdate, img) VALUES ('user_id', 'postdate', 'img')", [
-					'user_id'=>$userid,
-					'postdate'=>$postdate,
-					'img'=>$filename,
+			$connection->setInsert("INSERT INTO POST (user_id, postdate, img) VALUES (:user_id, :postdate, :img)", [
+					':user_id'=>$userid,
+					':postdate'=>$postdate,
+					':img'=>$filename,
 			]);
 			
 			setRedirect("./profile.php");
